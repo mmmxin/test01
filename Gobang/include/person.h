03ee27952c49bef4f13a6b1a2f2b6e00fc68fc41
penggy <pengguoyang@foxmail.com>
2021-10-27 15:24:53 +0800

    add the header of person

---
+++ Gobang/include/person.h
@@ -0,0 +1,88 @@
+#ifndef _PERSON_H
+#define _PERSON_H
+#include"checkerboard.h"
+
+/***************Macro definition***********************/
+#define DIRECTION 	INT
+#define VERTICAL 	1
+#define HORIZON  	2
+#define FSLASH		3	//forward slash
+#define BSLASH		4	//back slash
+
+/*****************************************************/
+//
+//
+//
+/*********************abstract class Cpeople*****************************/
+class Cpeople{
+	public:
+		Cpeople(int iId);
+		//This is the constructor;
+		//main job is to initialize m_peopleID;
+
+		_STATUS m_fnEye(Ccheckerboard myBoard,int x_label,int y_label,int& evalue) const;
+	        //to imitate the eye of people to see the board;
+		//input an object of the class(Ccheckerboard) which is the checkerboard need to be seen;
+		//input the x_label and y_label of the checkerboard;
+		//input evalue need te be read is the value of the vector of the member of the object of the Ccheckerboard;
+
+		virtual _STATUS m_fnHand(Ccheckerboard myBoard,int x_label,int y_label,int hvalue) = 0;
+		//to imitate the hand of people to play the game;
+		//input an object of the class(Ccheckerboard) which is the checkerboard need to be seen;
+		//input the x_label and y_label of the checkerboard;
+		//input hvalue need te be write to the value of the vector of the member of the object of the Ccheckerboard;
+		//this is a pure virtual function becasue of the different of the judge and the player;
+		
+
+		_STATUS m_fnConsiCount(Ccheckerboard myBoard,int x_label,int y_label,DIRECTION mscroDir,int& numOfCount);
+		//to count the number of the piece of the location(depend on the x_label and the y_label) of the checkerboard;
+		//input an object of the class(Ccheckerboard) which is the checkerboard need to be count;
+		//input the x_label and y_label of the checkerboard;
+		//input the DIRECTION is to descrebe the direction;
+		//the numOfCount which we need is the number of the piece in that direction;
+
+	protected:
+		int m_peopleId;
+
+};
+
+/************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
